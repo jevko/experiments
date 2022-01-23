e318e1854499fa56aa63bbbf88425d675430af5d
Dariusz Jędrzejczak <dariusz.jedrzejczak.work+github@gmail.com>
2022-01-23 23:55:56 +0100

    improve astToXml3

--- astToXml3.js
+++ astToXml3.js
@@ -3,9 +3,23 @@ export const astToXml3 = (ast) => {
   
   let ret = ''
   for (const {prefix, value} of subvalues) {
-    const tag = prefix.trim()
-    if (tag === '') astToXml3(value)
-    else ret += `<${tag}>${astToXml3(value)}</${tag}>`
+    const [pre, tag, post] = trim(prefix)
+    if (tag === '') ret += astToXml3(value)
+    else ret += `${pre}<${tag}>${astToXml3(value)}</${tag}>`
   }
   return ret + suffix
+}
+const trim = (prefix) => {
+  let i = 0, j = 0
+  for (; i < prefix.length; ++i) {
+    if (isWhitespace(prefix[i]) === false) break
+  }
+  for (j = i; j < prefix.length; ++j) {
+    if (isWhitespace(prefix[j])) break
+  }
+  return [prefix.slice(0, i), prefix.slice(i, j), prefix.slice(j)]
+}
+
+const isWhitespace = (c) => {
+  return ' \n\r\t'.includes(c)
 }
